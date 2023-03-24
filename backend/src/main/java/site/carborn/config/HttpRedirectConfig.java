2a76a4502d7cb61ce7d4be6251c12588ac3a97e3
rigizer <rigizer@gmail.com>
2023-03-24 16:52:34 +0900

    [BE] UPDATE : HttpRecirectConfig 재활성화

--- backend/src/main/java/site/carborn/config/HttpRedirectConfig.java
+++ backend/src/main/java/site/carborn/config/HttpRedirectConfig.java
@@ -12,30 +12,30 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class HttpRedirectConfig {
-//    @Value("${server.http.port}")
-//    private int serverPortHttp;
-//
-//    @Bean
-//    public ServletWebServerFactory servletContainer() {
-//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint = new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection = new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//
-//        tomcat.addAdditionalTomcatConnectors(createSslConnector());
-//        return tomcat;
-//    }
-//
-//    private Connector createSslConnector() {
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setPort(serverPortHttp);
-//        return connector;
-//    }
+    @Value("${server.http.port}")
+    private int serverPortHttp;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(serverPortHttp);
+        return connector;
+    }
 }
