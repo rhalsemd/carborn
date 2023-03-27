fbcdbabc992dca305f27be9f80e0e24e0997d6ad
rigizer <rigizer@gmail.com>
2023-03-27 15:10:10 +0900

    [BE] ADD : 회원가입 및 비밀번호 변경시 SMS 인증을 위한 SMSAuthRepository 생성

---
+++ backend/src/main/java/site/carborn/repository/common/SmsAuthRepository.java
@@ -0,0 +1,13 @@
+package site.carborn.repository.common;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import site.carborn.entity.common.SmsAuth;
+
+@Repository
+public interface SmsAuthRepository extends JpaRepository<SmsAuth, Integer> {
+    @Query(value = "SELECT * FROM MWS_SMS_AUTH WHERE (TIMESTAMPDIFF(MINUTE, NOW(), EXP_DT) BETWEEN 0 AND 3) and PHONE_NM = :phoneNm and STATUS = 1 ORDER BY EXP_DT DESC LIMIT 1;", nativeQuery = true)
+    SmsAuth checkSmsAuth(@Param("phoneNm") String phoneNm);
+}
