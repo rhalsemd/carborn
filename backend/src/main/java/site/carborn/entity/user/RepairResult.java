9d7d51dc778a41c67af991c1838b4acb6a324b9a
rigizer <rigizer@gmail.com>
2023-04-05 11:06:30 +0900

    [BE] UPDATE : Board Entity 삭제로 인한 import 수정

--- backend/src/main/java/site/carborn/entity/user/RepairResult.java
+++ backend/src/main/java/site/carborn/entity/user/RepairResult.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.beans.BeanUtils;
-import site.carborn.dto.request.RepairResultRequestDTO;
-import site.carborn.entity.board.Board;
 
 import java.time.LocalDateTime;
 
