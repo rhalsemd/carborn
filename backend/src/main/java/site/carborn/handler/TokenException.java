a3c15ccd998706554f38c1adb478b241a1a885c4
rigizer <rigizer@gmail.com>
2023-04-06 17:00:31 +0900

    [BE] UPDATE : 토큰 정보가 만료된 경우에 대한 오류대응 (TokenException)

---
+++ backend/src/main/java/site/carborn/handler/TokenException.java
@@ -0,0 +1,11 @@
+package site.carborn.handler;
+
+public class TokenException extends RuntimeException {
+    public TokenException() {
+        super();
+    }
+
+    public TokenException(String message) {
+        super(message);
+    }
+}
